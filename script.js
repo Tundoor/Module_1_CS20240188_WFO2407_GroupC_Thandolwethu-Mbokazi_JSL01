6318a3b1399e4ed521296883fc381c09ab55d8d6
Thandolwethu <thandwem045@gmail.com>
2024-08-29 20:09:24 +0200

    Updated JavaScript

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 function validateSyntax() {
     let input = document.getElementById('petInput').value;
     let result = '';
-    let alphaNumeric = new RegExp(/^[A-Za-z0-9]+$/i);                          
-      if (input.startsWith("pet_") && alphaNumeric.test(input.slice(4))) {    // Validates whether alphanumeric and starts with pet_
+    let alphaNumeric = new RegExp(/^[A-Za-z0-9]+$/i); 
+    let colourChange = document.getElementById("result")                         
+      
+    if (input.startsWith("pet_") && alphaNumeric.test(input.slice(4))) {    // Validates whether alphanumeric and starts with pet_
+        colourChange.classList.add('valid')
         result = "Valid Syntax";
+        
       } else {
-        result = "Invalid Syntax";
-      }
+        colourChange.classList.add('invalid') 
+          result = "Invalid Syntax";
+      };
 
 
     document.getElementById('result').innerText = result;       // Reassigns the result varieble so it's displayed
-}
+};
 
 
