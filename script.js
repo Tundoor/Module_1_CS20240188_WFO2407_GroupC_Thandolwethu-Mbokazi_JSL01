0f67f2dd326ce30804602e4baa2a604fffa36a39
Thandolwethu <thandwem045@gmail.com>
2024-08-29 19:23:36 +0200

    Updated validation in JavaScript

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 function validateSyntax() {
     let input = document.getElementById('petInput').value;
-    let result = ''; // Placeholder for validation result
-      if (input.startsWith("pet_")) {
+    let result = '';
+    let alphaNumeric = new RegExp(/^[A-Za-z0-9]+$/i);                          
+      if (input.startsWith("pet_") && alphaNumeric.test(input.slice(4))) {    // Validates whether alphanumeric and starts with pet_
         result = "Valid Syntax";
       } else {
         result = "Invalid Syntax";
       }
-    
 
-         document.getElementById('result').innerText = result;
+
+    document.getElementById('result').innerText = result;       // Reassigns the result varieble so it's displayed
 }
 
 
