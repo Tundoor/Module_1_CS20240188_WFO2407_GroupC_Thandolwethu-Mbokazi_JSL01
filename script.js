335ccf5d7a711e2b23c886c2233f56d6660af806
Thandolwethu <thandwem045@gmail.com>
2024-08-30 17:15:24 +0200

    Added comments

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function validateSyntax() {
     let input = document.getElementById('petInput').value;
     let result = '';
-    let alphaNumeric = new RegExp(/^[A-Za-z0-9]+$/i); 
+    let alphaNumeric = new RegExp(/^[A-Za-z0-9]+$/i); //expression for alphanumeric numbers
     let colourChange = document.getElementById("result")                         
       
     if (input.startsWith("pet_") && alphaNumeric.test(input.slice(4))) {    // Validates whether alphanumeric and starts with pet_
-        colourChange.classList.add('valid')
+        colourChange.classList.add('valid')  //changes the colour of the text
         result = "Valid Syntax";
         
       } else {
-        colourChange.classList.add('invalid') 
+        colourChange.classList.add('invalid') //changes colour of the text
           result = "Invalid Syntax";
       };
 
